ee17947ca66646d0ca5fd81858c02bc387de51d6
Aryan Singh <96579866+AryanSinghIIIT@users.noreply.github.com>
2022-05-17 23:18:56 +0530

    Add files via upload

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter, AiFillLinkedin} from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2022 All Things More Store All rights reserverd</p>
+      <p className="icons">
+      <a href={'https://www.linkedin.com/in/aryan-singh-b01832193/'}><AiFillLinkedin  /> About Us</a>
+
+        
+      </p>
+    </div>
+  )
+}
+
+export default Footer
